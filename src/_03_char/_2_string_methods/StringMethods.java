3c01ad72926fce3d8219223bcc7530d3cde78f7a
dugga <dugga@MSI>
2022-11-30 20:01:50 -0800

    Finished both of these in the staged changes.

--- src/_03_char/_2_string_methods/StringMethods.java
+++ src/_03_char/_2_string_methods/StringMethods.java
@@ -1,29 +1,35 @@
 package _03_char._2_string_methods;
 
+import javax.swing.JOptionPane;
 
 public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to contain a minimum of 3 characters
-
+		
+String Hello = "Hello" ; 
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
-
+             Hello.charAt(2);
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+Hello.length();
 		
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
-
+String variable = "Hello" ;
+for(int i = 0; i < Hello.length(); i++ ) {
+Hello.charAt(i);
+System.out.println(Hello.charAt(i)+ " is at index " + i);
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
-		
+}
+
 	}
 }
 
--- src/_03_char/_3_pi_aloud/PiAloud.java
+++ src/_03_char/_3_pi_aloud/PiAloud.java
@@ -2,37 +2,50 @@ package _03_char._3_pi_aloud;
 
 import java.util.Scanner;
 
+import game_tools.Sound;
+
 public class PiAloud {
     /***********  SOUND ***************
      * Some computers are unable to play sounds.
      * If you cannot play sound on this computer, set canPlaySounds to false.
      * If you are not sure, ask your teacher
      * *****************/
-    boolean canPlaySounds = true;
+    static boolean canPlaySounds = true;
+    public static void main(String[] args) {
+    	String Mellow = "3.14159265358979323846" ;
 
 
-    // 1. Make a main method and make sure your program can run
+    	// 1. Make a main method and make sure your program can run
 
-    // 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
+    	// 2. Make a String variable to hold 20 digits of Pi. You could use http://www.piday.org/million/ to get this.
 
-    // 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-    // Check the console shows 3.1
+    	// 3. Print out 3 digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
+    	// Check the console shows 3.1
 
-    // 4. Print ALL the digits of the Pi String, putting each digit on a new line  (hint: use a loop)
+    	// 4. Print ALL the digits of the Pi String, putting each digit on a new line  (hint: use a loop)
+    	
+    	for(int i = 0; i < Mellow.length(); i++) {
+    		Mellow.charAt(i);
+    		System.out.println(Mellow.charAt(i));
+    		if (canPlaySounds) {
+    			Sound.speak(Mellow);
+    		}
+    	}
 
-    // 5. Skip this step if your computer cannot play sounds.
-    //if (canPlaySounds) {
-        // Use the Sound.speak() method to speak each digit of Pi.
-        
-    //}
+    	// 5. Skip this step if your computer cannot play sounds.
+    	//if (canPlaySounds) {
+    	// Use the Sound.speak() method to speak each digit of Pi.
+      
+       
+    	//}
 
 
-    // [CHALLENGE]
-    // *6. Get a character from the user using the getInputFromUser() method
-    // *7. Compare the users' char to the next digit of Pi
-    // *8. If they are correct, print out "correct". If they are not, print "incorrect"
+    	// [CHALLENGE]
+    	// *6. Get a character from the user using the getInputFromUser() method
+    	// *7. Compare the users' char to the next digit of Pi
+    	// *8. If they are correct, print out "correct". If they are not, print "incorrect"
 
-
+    }
     /********************  Use these methods. DON'T CHANGE THE CODE BELOW  ******************/
 
     private static void pause(int seconds) {
