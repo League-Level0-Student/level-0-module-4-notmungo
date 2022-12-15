84421d9f2f4fccfa213ceb46783910497637e3a0
dugga <dugga@MSI>
2022-12-14 19:51:00 -0800

    Finished fruit quiz almost finished simple calculator

--- src/_99_extra/FruitQuiz.java
+++ src/_99_extra/FruitQuiz.java
@@ -13,13 +13,17 @@ public class FruitQuiz extends KeyAdapter {
 	void makeQuestions() {
 		question1 = new JLabel("<html>Which is not a real fruit? <br> A: Dragon Fruit <br> B: Durian <br> C: Crazyberry</html>");
 		// 11. Make another question called "question2".  Use question1 above as a guide.
+	    question2 = new JLabel("html>Which is not a real place? <br> A: Germany <br> B: Gnebahsbia <br> C: France");
 	}
 
 	@Override
 	public void keyPressed(KeyEvent arg0) {
 		int keyCode = arg0.getKeyCode();
+		System.out.println(keyCode);
 		// 1. Print out the key code variable
-
+int a =65;
+int b =66;
+int c =67;
 		// 2. Make 3 int variables that hold the key codes for A, b, and C
 		
 		// 14. Repeat steps 11, 12, and 13 for question3 and question4 - IMPORTANT: The questions must be in reverse order from top to bottom to work properly
@@ -31,7 +35,13 @@ public class FruitQuiz extends KeyAdapter {
 			
 		if (question1.isShowing()) {
 			// 3. If they selected the right fruit, do steps 4 and 7
-			
+			if(keyCode == a) {
+				correct();
+				nextQuestion(question2);
+			}
+			else {
+				incorrect();
+			}
 				// 4. Call the correct() method
 				
 				// 7. Use the nextQuestion() method to go to question2
@@ -50,13 +60,13 @@ public class FruitQuiz extends KeyAdapter {
 		// the 'extra' package. It must be a .wav file. 
 		// There are lots on freesound.org
 		// 6. Use the playSound method to play your sound
-
+playSound("correct.wav");
 
 	}
 
 	private void incorrect() {
 		// 10. Find a sound for wrong answers and put it in the default package. Use the playSound method to play it.
-
+playSound("wrong.wav");
 	}
 
 	private void nextQuestion(JLabel newQuestion) {
--- src/_99_extra/SimpleCalculator.java
+++ src/_99_extra/SimpleCalculator.java
@@ -7,7 +7,8 @@ public class SimpleCalculator {
 	public static void main(String[] args) {
 
 		// 1. Get 2 numbers from the user and convert them to integer.
-
+String Calc = JOptionPane.showInputDialog("Give me a number");
+String Calc1 = JOptionPane.showInputDialog("Give me a number");
 		// 2. Customize pop-up to support add/subtract/multiply/divide operations.
 		int operation = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
 				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Button1", "Button2", "Button3", "Button3" },
