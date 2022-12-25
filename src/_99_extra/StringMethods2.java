eea29e9600941a114463f70dd24e4ae345296e26
JeremyCui2008 <jeremycuid@gmailcom>
2022-12-24 17:55:12 -0800

    Finished stringmethods2 and almost finsihed the creeper project, super close to being down with it completely .

--- src/_99_extra/StringMethods2.java
+++ src/_99_extra/StringMethods2.java
@@ -6,18 +6,26 @@ public class StringMethods2 {
 		
 		// 1. Create a String variable and initialize it with
 		//    random upper and lower case characters.
+String mungo = "kjJsJSngfN";
 
 		// 2. Print your String to the console in upper case.
-		
-		// 3. Print your String to the console in lower case.
 
+System.out.println(mungo.toUpperCase());
+
+		// 3. Print your String to the console in lower case.
+System.out.println(mungo.toLowerCase());
 		// 4. Print the first 3 char's of your String
 		//    HINT: .substring(start,end)
-		
+        System.out.println(mungo.substring(0,3));
+        
 		// 5. Print a single char somewhere in the middle of your String
 		//    REMINDER: char's in string start at index 0
-		
+		System.out.println(mungo.charAt(2));
 		// 6. BONUS -- print the LAST 3 char's of your string using
 		//        .length() to determine WHERE the last 3 char's are located.
+		System.out.println(mungo.substring(mungo.length()-3));
+	
+			
+		
 	}
 }
--- src/_99_extra/hide_the_creeper/HideTheCreeper.java
+++ src/_99_extra/hide_the_creeper/HideTheCreeper.java
@@ -1,24 +1,37 @@
 package _99_extra.hide_the_creeper;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class HideTheCreeper extends PApplet {
     static final int WIDTH = 600;
     static final int HEIGHT = 400;
-
+PImage creeper;
+int x=1;
+int y=100;
     @Override
     public void settings() {
-        size(WIDTH, HEIGHT);
+        size(600, 400);
     }
 
     @Override
     public void setup() {
-        
+    	
+    	PImage minecraft = loadImage("minecraft.jpeg"); 
+    	minecraft.resize(WIDTH, HEIGHT);
+    	background(minecraft);
+    	creeper=loadImage("creeper.jpeg");
+    	creeper.resize(4, 8);
+    	
     }
 
     @Override
     public void draw() {
-        
+    image(creeper,x,y);
+    fill(250,0,3);
+    ellipse(2,3,4,5);
+    
+    
     }
 
     static public void main(String[] args) {
