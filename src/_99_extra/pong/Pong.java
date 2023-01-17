a349e30af8dd7625985c490af9095359e5c419b5
dugga <dugga@MSI>
2023-01-16 20:04:56 -0800

    Almost finished pong.java, super close to being done with it.

--- src/_99_extra/pong/Pong.java
+++ src/_99_extra/pong/Pong.java
@@ -6,6 +6,10 @@ public class Pong extends PApplet {
     static final int WIDTH = 800;
     static final int HEIGHT = 600;
     int x = 50;
+    int y = 50;
+    int ySpeed = 5;
+    int xSpeed = 5;
+    
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
@@ -13,15 +17,25 @@ public class Pong extends PApplet {
 
     @Override
     public void setup() {
-
+    	
     }
 
     @Override
     public void draw() {
-    	
-    	ellipse(x, 50, 90, 80);
+    	background(255,255,255);
+    	ellipse(x, y, 90, 80);
     	fill(125, 135, 250) ;
     	stroke(125, 135, 250);
+    	y+= ySpeed;
+    	
+    	x += xSpeed ;
+    	if(x > width && y > height){
+    		ySpeed = -ySpeed;
+    	    xSpeed = -xSpeed;
+    	}
+    	
+    	rect(mouseX, 560, 150,50);
+    	
     	
     }
 
